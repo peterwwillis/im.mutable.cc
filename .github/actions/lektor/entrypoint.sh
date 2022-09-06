2c79ca1eb6494bd28f60f345fcabab6a48c85a14
Peter Willis <31324861+peterwwillis@users.noreply.github.com>
2022-09-06 02:56:13 -0400

    Forgot to export vars

--- .github/actions/lektor/entrypoint.sh
+++ .github/actions/lektor/entrypoint.sh
@@ -3,12 +3,12 @@ set -eux
 
 if [ -n "$INPUT_DEPLOY_USERNAME" ] ; then
     # shellcheck disable=SC2034
-    LEKTOR_DEPLOY_USERNAME="$INPUT_DEPLOY_USERNAME"
+    export LEKTOR_DEPLOY_USERNAME="$INPUT_DEPLOY_USERNAME"
 fi
 
 if [ -n "$INPUT_DEPLOY_PASSWORD" ] ; then
     # shellcheck disable=SC2034
-    LEKTOR_DEPLOY_PASSWORD="$INPUT_DEPLOY_PASSWORD"
+    export LEKTOR_DEPLOY_PASSWORD="$INPUT_DEPLOY_PASSWORD"
 fi
 
 if [ -n "$INPUT_MAKEARG" ] ; then
